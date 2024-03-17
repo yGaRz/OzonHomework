574875441c821fb1c45fa01b228aede64948867b
yGaR <ygar@rambler.ru>
2024-03-17 23:41:55 +0300

    add xUnit project.

---
+++ TestServices/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Ozon.Route256.Practice.GatewayService;
+using Ozon.Route256.Practice.GatewayService.Models;
+namespace TestServices
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            PaginationParameter paginationParameter = new PaginationParameter();
+            paginationParameter.PageSize = 70;
+            paginationParameter.PageIndex = 6;
+            
+        }
+    }
+}
