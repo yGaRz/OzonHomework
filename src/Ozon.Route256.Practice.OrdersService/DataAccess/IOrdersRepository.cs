68eefe7f6d469325917a31d42c5f41dd9999e1b8
yGaR <ygar@rambler.ru>
2024-03-19 23:16:09 +0300

    Перед слиянием заливаю все в ветку.

--- src/Ozon.Route256.Practice.OrdersService/DataAccess/IOrdersRepository.cs
+++ src/Ozon.Route256.Practice.OrdersService/DataAccess/IOrdersRepository.cs
@@ -3,9 +3,21 @@ namespace Ozon.Route256.Practice.OrdersService.DataAccess
 {
     public interface IOrdersRepository
     {
+        //Добавление заказа в репозиторий
         Task CreateOrderAsync(OrderEntity order, CancellationToken token = default);
-        Task<OrderEntity> GetOrderById(int id, CancellationToken token = default);
-
-
+        //Выборка заказов по идентификатору
+        Task<OrderEntity> GetOrderByCustomer(int id, CancellationToken token = default);
+        //Смена состояния заказа
+        Task<bool> SetOrderStateAsync(int id, OrderState state, CancellationToken token = default);
+        //выборка списка заказов
+        Task<List<OrderEntity>> GetOrdersByRegionAsync(List<string> regionList, 
+                                                            OrderSource source, 
+                                                            SortParam param=SortParam.None, 
+                                                            string  field_name="", 
+                                                            CancellationToken token = default);
+        //Получение агегации(статистики) заказов по региону
+        Task<List<RegionStatisticEntity>> GetRegionsStatisticAsync(List<string> regionList, DateTime dateStart, CancellationToken token = default);
+        //Получение всех заказов клиента
+        Task<List<OrderEntity>> GetOrdersByCutomer(ulong idCustomer, DateTime dateStart, CancellationToken token = default);
     }
 }
