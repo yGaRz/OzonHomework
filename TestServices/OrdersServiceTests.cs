db8e15e3dde041a27f56e89c9c8b56093726aee8
yGaR <ygar@rambler.ru>
2024-03-25 14:37:31 +0300

    update first mock

--- TestServices/OrdersServiceTests.cs
+++ TestServices/OrdersServiceTests.cs
@@ -9,15 +9,6 @@ namespace TestServices;
 
 public class OrdersServiceTests
 {
-    //public override async Task<GetRegionResponse> GetRegion(GetRegionRequest request, ServerCallContext context)
-    //{
-    //    var regions = await _regionRepository.GetRegionsAsync();
-    //    var result = new GetRegionResponse
-    //    {
-    //        Region = { regions.ToArray() }
-    //    };
-    //    return result;
-    //}
 
     [Fact]
     public async Task TestGetReion1()
@@ -26,18 +17,18 @@ public class OrdersServiceTests
         var context = TestServerCallContext.Create();
 
         mockRegion.Setup(m => m.GetRegionsAsync(context.CancellationToken)).ReturnsAsync(
-                            (string[] s) => {
+                            () => {
                                 string[] regions = { "Moscow", "StPetersburg", "Novosibirsk"};
                                 return regions;
                             });
 
         var service = new OrdersService(mockRegion.Object,null,null,null);
 
-        var request = new Ozon.Route256.Practice.GetRegionRequest() { };
-        
+        var request = new Ozon.Route256.Practice.GetRegionRequest() { };        
         var regionResponce = await service.GetRegion(request, context);
+        Assert.NotNull(regionResponce);
         Assert.Contains("Moscow",regionResponce.Region);
-
+        Assert.DoesNotContain("London", regionResponce.Region);
     }
 
 }
