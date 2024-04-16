9e5f420a294eedebb9060ca374f47366d1ee706b
yGaR <ygar@rambler.ru>
2024-04-16 18:03:00 +0300

    up2

--- src/Ozon.Route256.Practice.OrdersService.Infrastructure/Startup.cs
+++ src/Ozon.Route256.Practice.OrdersService.Infrastructure/Startup.cs
@@ -1,11 +1,11 @@
 ﻿﻿using FirebirdSql.Data.Services;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Ozon.Route256.Practice.OrdersService.Infrastructure.ClientBalancing;
 using Ozon.Route256.Practice.OrdersService.Infrastructure.DAL.Common;
 using Ozon.Route256.Practice.OrdersService.Infrastructure.DAL.Shard.Common;
 using Ozon.Route256.Practice.OrdersService.Infrastructure.DAL.Shard.Common.Rules;
 using Ozon.Route256.Practice.SdServiceGrpcFile;
->>>>>>>>> Temporary merge branch 2
 
 namespace Ozon.Route256.Practice.OrdersService.Infrastructure;
 
